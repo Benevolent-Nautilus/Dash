ba4e7d63725b08efd54e10ad90623088caf714b2
Derek Van Dyke <haoled@gmail.com>
2015-04-11 16:47:40 -0700

    (feat) Add mainpage routing fix #34

--- server/app.js
+++ server/app.js
@@ -7,10 +7,19 @@
 
 */
 var express = require('express');
+var path = require('path');
 var app = express();
 var port = 8080;
 
-require('./routes')(app);
+//Setting views to correct directory
+app.set('views', path.join(__dirname, '../client/dist/'));
+app.set('view engine', 'ejs');
+
+//Serving mainpage
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//Using ejs as template engine
+app.engine('html', require('ejs').renderFile);
 
 app.listen(port);
 console.log('Listening on port: ', port);
