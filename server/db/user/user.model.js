4c716a9457fa709e0e52a5089e80fbee1d121f9b
sunyang730 <sunyang730@gmail.com>
2015-04-13 15:09:34 -0700

    (feat) add user schema and model fix #28

---
+++ server/db/user/user.model.js
@@ -0,0 +1,45 @@
+//Create user schema using mongoose
+//author: Dennis Yang
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name: {
+    first: {type: String},
+    last: {type: String}
+  },
+  emailAddress: {type: String, unique: true},
+  fitnessDevice: {
+    token: {type: String},
+    tokenSecret: {type: String},
+  },
+  oauth: {
+    facebook: {
+      id: {type: String, unique: true},
+      token: {type: String},
+      tokenSecret: {type: String}
+    },
+    google: {
+      id: {type: String, unique: true},
+      token: {type: String},
+      tokenSecret: {type: String}
+    },
+    twitter: {
+      id: {type: String, unique: true},
+      token: {type: String},
+      tokenSecret: {type: String}
+    }
+  },
+  session: {type: String},
+  steps: {
+    dailySteps: {type: Number},
+    totalSteps: {type: Number}
+  },
+  friends: [{type: Schema.Types.ObjectId, ref: 'User'}],
+  challenges: [{type: Schema.Types.ObjectId, ref: 'Challenge'}]
+});
+
+
+module.exports = mongoose.model('User', userSchema); 
