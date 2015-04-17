a482cb420bfaf8ec7075d33cce845deb44dec229
Scott Kao <scottkao85@gmail.com>
2015-04-17 12:03:08 -0700

    add: single stat component for individual challenge

--- client/src/scripts/components/challenges/ChallengeStat.jsx
+++ client/src/scripts/components/challenges/ChallengeStat.jsx
@@ -1,33 +1,17 @@
-// // Reflux
-// var Reflux = require('reflux');
-// // Router
-// var Router = require('react-router');
-// // Actions
-// var actions = require('../../actions/actions');
+// Reflux
+var Reflux = require('reflux');
+// Router
+var Router = require('react-router');
 
+var ChallengeStat = React.createClass({
+  render: function(){
+    console.log(this.props);
+    return (
+      <li> {this.props.data.name} | {this.props.data.totalSteps} 
+      </li>
+      );
+  }
 
+})
 
-// var challenges = ["Frodo's Journey to Mt Doom", 15000000, 17790000] 
-
-// var ChallengeStat = React.createClass({
-//   getInitialState: function() {
-//     return {
-//       ChartData: {
-//         challenge_Name : challenges[0],
-//         participants: [ 
-//           {
-//             name: 'Scott Kao',
-//             current_step: 15000000
-//           },  
-//           {
-//             name: 'Dennis Yang',
-//             current_step: 15256000
-//           }
-//         ], 
-//         challenges_goal: challenges[2]
-//       }
-//     }
-//   }
-// });
-
-//   module.exports = ChallengeStat;
+module.exports = ChallengeStat;
