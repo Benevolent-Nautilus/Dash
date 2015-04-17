6e57399d9c233dcd6f9a1a90dc985a930300646a
Scott Kao <scottkao85@gmail.com>
2015-04-17 15:09:28 -0700

    fix: able to render challenges component from dummy data

--- client/src/scripts/components/challenges/ChallengeStat.jsx
+++ client/src/scripts/components/challenges/ChallengeStat.jsx
@@ -5,9 +5,13 @@ var Router = require('react-router');
 
 var ChallengeStat = React.createClass({
   render: function(){
-    console.log(this.props);
+    var props = this.props.data
+    for(var i = 0; i < props.length; i++){
+      
+    }
     return (
-      <li> {this.props.data.name} | {this.props.data.totalSteps} 
+      <li> {this.props.data[0].name} | {this.props.data[0].currentSteps} / {this.props.data[0].totoalSteps}
+       <div>{this.props.data[0].friends[0].name}</div>
       </li>
       );
   }
--- client/src/scripts/components/challenges/ChallengeStats.jsx
+++ client/src/scripts/components/challenges/ChallengeStats.jsx
@@ -1,33 +1,21 @@
-// // Reflux
-// var Reflux = require('reflux');
-// // Router
-// var Router = require('react-router');
-// // Actions
-// var actions = require('../../actions/actions');
+// Reflux
+var Reflux = require('reflux');
+// Router
+var Router = require('react-router');
+// Actions
+var actions = require('../../actions/actions');
 
 
-
-// var challenges = ["Frodo's Journey to Mt Doom", 15000000, 17790000] 
-
-// var ChallengeStat = React.createClass({
-//   getInitialState: function() {
-//     return {
-//       ChartData: {
-//         challenge_Name : challenges[0],
-//         participants: [ 
-//           {
-//             name: 'Scott Kao',
-//             current_step: 15000000
-//           },  
-//           {
-//             name: 'Dennis Yang',
-//             current_step: 15256000
-//           }
-//         ], 
-//         challenges_goal: challenges[2]
-//       }
-//     }
-//   }
-// });
-
-//   module.exports = ChallengeStat;
+var ChallengeStats = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ul>
+          {this.prop.data.map(function(stat){
+            return <li key=(stat.id)> <ChallengeStat data= { stat }>
+          })}
+        </ul>
+      </div>
+    );
+  };
+});
