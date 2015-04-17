dd71c6e72d1cabf1e7f22a32dea4a68e75d1f06c
Scott Kao <scottkao85@gmail.com>
2015-04-16 21:46:20 -0700

    modify: user participating challenges view

--- client/src/scripts/views/challenges/ParticipatingChallenges.jsx
+++ client/src/scripts/views/challenges/ParticipatingChallenges.jsx
@@ -5,28 +5,18 @@ var Router = require('react-router');
 
 // Components
 var Spinner = require('../../components/spinner');
-var Dashboard = require('../../components/Profile/Dashboard');
+var ChallengeStat = require('../../components/challenges/ChallengeStat');
 var Footer = require('../../components/Profile/Footer');
 
 
-var challenges = ["Frodo's Journey to Mt Doom", 15000000, 17790000] 
 
 var ParticipatingChallenges = React.createClass({
-  getInitialState: function() {
-    return {
-      ChartData: {
-        challenge_Name : challenges[0],
-        steps_completed: challenges[1],
-        challenges_goal: challenges[2],
-      }
-    }
-  },
 
   render: function() {
     console.log("hello");
     return (
       <div className="text-center">
-        < Dashboard />
+         < ChallengeStat />
         < Footer />
         
       </div>
