e56d14937d3032ae98b95acf0ef2203ffc14bbd6
Scott Kao <scottkao85@gmail.com>
2015-04-17 23:25:51 -0700

    fix: connet components to action/store

--- client/src/scripts/components/challenges/AvailableChallenge.jsx
+++ client/src/scripts/components/challenges/AvailableChallenge.jsx
@@ -2,17 +2,15 @@
 var Reflux = require('reflux');
 // Router
 var Router = require('react-router');
+var ChallengeStore = require('../../stores/challengeStore')
 
-var selectChallenge = function(e){
-  e.preventDefault();
-  console.log('clicked on new challenge!')
-}
 
 var AvailableChallenge = React.createClass({
   render: function(){
-    console.log(this.props);
+    console.log(this.onAdd);
     return (
-        <li onClick={ this.selectChallenge }> {this.props.data.name} | {this.props.data.totalSteps} 
+        <li > {this.props.data.name} | {this.props.data.totalSteps} 
+          <button onClick={this.onAdd}> select challenge </button>                  
         </li>
     );
   }
@@ -20,3 +18,10 @@ var AvailableChallenge = React.createClass({
 })
 
 module.exports = AvailableChallenge;
+
+
+
+// // var selectChallenge = function(e){
+//   e.preventDefault();
+//   console.log('clicked on new challenge!')
+// }
--- client/src/scripts/views/challenges/NewChallenges.jsx
+++ client/src/scripts/views/challenges/NewChallenges.jsx
@@ -2,37 +2,22 @@
 var Reflux = require('reflux');
 // Router
 var Router = require('react-router');
+var ChallengeStore = require('../../stores/challengeStore')
 
 // Components
 var Spinner = require('../../components/spinner');
 var Footer = require('../../components/Profile/Footer');
 var AvailableChallenges = require('../../components/Challenges/AvailableChallenges');
 
-var challengeList = [
-                  {
-                    name: 'Everest',
-                    totalSteps: 100000
-                  }, 
-                  {
-                    name: "Frodo's Journey to Mount Doom",
-                    totalSteps: 17000000
-                  }, 
-                  {
-                    name: "Great Wall of China",
-                    totalSteps: 1800000
-                  }
-    ] 
-
-
 var NewChallenges = React.createClass({
   mixins: [],
 
   getInitialState: function() {
-    return { data: challengeList };
+    return { data: ChallengeStore.fetchChallenges() };
   }, 
 
   render: function() {
-    console.log("new challenges");
+    console.log("data", this.state.data);
     return (
       <li className="text-center" >
         < AvailableChallenges data= { this.state.data } /> 
