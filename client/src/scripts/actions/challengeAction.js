b05db56c79effb59dede0983d0c7bc8f86849261
Scott Kao <scottkao85@gmail.com>
2015-04-17 20:56:47 -0700

    add: initial set up challenge action and store

---
+++ client/src/scripts/actions/challengeAction.js
@@ -0,0 +1,7 @@
+var Reflux = require('reflux');
+
+var ChallengeAction = Reflux.createAction([
+  'selectChallenge'
+]);
+
+module.exports = ChallengeAction;
---
+++ client/src/scripts/stores/challengeStore.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ChallengeAction = require('../actions/ChallengeAction');
+
+var _challenges = [];
+
+var ChallengeStore = Reflux.createStore({
+  init: function(){
+    
+  }
+})
