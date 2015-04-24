3499b0cca4d4eadc7f0b199be6503d4f7b1c35fe
sunyang730 <sunyang730@gmail.com>
2015-04-24 14:46:02 -0700

    (refactor) token time - add more time for token

--- server/auth/facebook/index.js
+++ server/auth/facebook/index.js
@@ -20,7 +20,7 @@ router
     session: false
   }), function(req, res){
     //setting the token for the cookie
-    var token = jwt.sign({id:req.user._id}, secret , {expiresInMinutes: 60});
+    var token = jwt.sign({id:req.user._id}, secret , {expiresInMinutes: 1440 * 360});
     res.cookie('token', JSON.stringify(token));
 
     //redirect to register device if new user, else go to profile
