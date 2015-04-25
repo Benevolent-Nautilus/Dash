282d25e1bea16e0fac66f441ccd16f816d4e2b27
Jason Chang <jasonjchang@icloud.com>
2015-04-25 15:03:35 -0700

    Updated Dashboard Challenges

--- client/src/scripts/components/profile/dashboard.jsx
+++ client/src/scripts/components/profile/dashboard.jsx
@@ -10,12 +10,14 @@ var actions = require('../../actions/actions');
 var Chart = require('./chart');
 var Tracker = require('./tracker');
 var Challenges = require('./challenges');
+var CurrentChallenge = require('../challenges/currentChallenge');
 
 var Dashboard = React.createClass({
 
   mixins: [],
 
   render: function() {
+    console.log('competitions', this.props.data.competitions);
     return (
       <div className="Application">
         <div className="fixed-tracker-dashboard">
@@ -23,11 +25,16 @@ var Dashboard = React.createClass({
               stepsToday = { this.props.data.stepsToday } 
               dailyGoal = { this.props.data.dailyGoal }/>
           <Tracker totalSteps = { this.props.data.totalSteps }/>
-          <Challenges competitions= { this.props.data.competitions } />
+          <ul className="current-challenges challenges-box-dash">
+            <CurrentChallenge key="12348" uid="12348" name="Everest" currentSteps="384729" goal="100000" amountOfFriends="5" />
+          </ul>
         </div>
       </div>
     );
   }
 });
 
+            // <CurrentChallenge key={stat.uid} uid={stat.uid} name={stat.name} currentSteps={stat.currentSteps} goal={stat.goal} amountOfFriends={stat.amountOfFriends} />
+
+// <Challenges competitions= { this.props.data.competitions } />
 module.exports = Dashboard;
